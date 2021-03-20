a143f264ca548aa60cb7d1dec8f77477b6555995
squarer <squarerliao@gmail.com>
2021-03-20 22:24:40 +0800

    theme

--- src/theme.js
+++ src/theme.js
@@ -14,9 +14,23 @@ const theme = createMuiTheme({
       main: red.A400,
     },
     background: {
-      default: '#eee',
+      default: '#f4f5f7',
     },
+    text: {
+      primary: '#575757',
+    }
   },
+  shadows: [
+    'none',
+    '0 2px 12px 0 rgba(0, 0, 0,.1)',
+    '0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)',
+    '0px 3px 3px -2px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 1px 8px 0px rgba(0,0,0,0.12)',
+    '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)',
+    '0px 3px 5px -1px rgba(0,0,0,0.2),0px 5px 8px 0px rgba(0,0,0,0.14),0px 1px 14px 0px rgba(0,0,0,0.12)',
+    '0px 3px 5px -1px rgba(0,0,0,0.2),0px 6px 10px 0px rgba(0,0,0,0.14),0px 1px 18px 0px rgba(0,0,0,0.12)',
+    '0px 4px 5px -2px rgba(0,0,0,0.2),0px 7px 10px 1px rgba(0,0,0,0.14),0px 2px 16px 1px rgba(0,0,0,0.12)',
+    '0 20px 50px 0 rgba(0, 0, 0, 0.14)',
+  ]
 });
 
 export default theme;
